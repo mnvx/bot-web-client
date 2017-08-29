4b9e82eafd84fe73e72e4727b0aabb7ce8ccc58a
mnv <mnvx@yandex.ru>
2017-08-30 01:12:31 +0300

    #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule, MdIconModule } from '@angular/material';
+import {
+  MdButtonModule,
+  MdCheckboxModule,
+  MdIconModule,
+  MdToolbarModule,
+  MdGridListModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { QaComponent } from './qa/qa.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QaComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
     MdIconModule,
+    MdToolbarModule,
+    MdGridListModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'qa', component: QaComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/qa/qa.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qa',
+  templateUrl: './qa.component.html',
+  styleUrls: ['./qa.component.css']
+})
+export class QaComponent {
+}
