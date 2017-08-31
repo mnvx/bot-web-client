c7144fd6d5f1bdcc71c4bca12c6f3cb01ea3d529
mnv <mnvx@yandex.ru>
2017-08-31 10:46:20 +0300

    #3 responsive menu

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-main-menu',
@@ -6,11 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
-  title = 'Смарт отель';
 
-  constructor() { }
+  /**
+   * Open or not menu for small devices
+   * @type {boolean}
+   */
+  public open = false;
+
+  /**
+   * Menu items
+   * @type Object
+   */
+  public links = [
+    {
+      key: "blog",
+      route: "",
+      title: "Блог",
+    },
+    {
+      key: "qa",
+      route: "qa",
+      title: "Вопросы и ответы",
+    },
+    {
+      key: "price",
+      route: "price",
+      title: "Цены",
+    },
+  ];
+
+  constructor(private router: Router) {
+    this.ngOnInit();
+  }
 
   ngOnInit() {
+    this.actualizeMenuVisibility();
+  }
+
+  onMenu() {
+    this.open = !this.open;
+    this.actualizeMenuVisibility();
+  }
+
+  protected actualizeMenuVisibility() {
+    let items = document.getElementsByClassName('main-sub-menu');
+    if (this.open) {
+      for (let i = 0; i != items.length; ++i) {
+        let item = <HTMLElement>items[i];
+        item.classList.add('open');
+        item.classList.remove('close');
+      }
+    }
+    else {
+      for (let i = 0; i != items.length; ++i) {
+        let item = <HTMLElement>items[i];
+        item.classList.remove('open');
+        item.classList.add('close');
+      }
+    }
+  }
+
+  onSelectMenuItem(item) {
+    this.router.navigate([item.route]);
+    console.log(item);
   }
 
 }
